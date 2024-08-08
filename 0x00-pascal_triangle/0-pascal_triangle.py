e028d19edc9e86d82ae5edebac97d5d9e7f9dcd4
MartinScript <martinsojoola2@gmail.com>
2024-08-08 22:42:10 +0100

    Refactor code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Generate Pascal's Triangle up to the nth row.
+
+Pascal's Triangle is a triangular array of binomial coefficients. Each number is the sum of the two directly above it.
+The function accepts an integer n as input and returns a list of lists representing the Pascal's Triangle up to the nth row."""
   
 def pascal_triangle(n):
     """
@@ -8,21 +13,18 @@ def pascal_triangle(n):
     The function accepts an integer n as input and returns a list of lists representing the Pascal's Triangle up to the nth row.
 
     Parameters:
-    n (int): The number of rows to generate in Pascal's Triangle.
+    n (int): The number of rows to generate in Pascal's Triangle. Must be a positive integer.
 
     Returns:
-    list: A list of lists representing Pascal's Triangle up to the nth row. If n is not a positive integer, an empty list is returned.
-
-    Raises:
-    ValueError: If n is not an integer.
+    list: A list of lists representing Pascal's Triangle up to the nth row. If n is not a positive integer, returns an empty list.
     """
     if type(n) is not int or n <= 0:
-      return [[]]
+        return [[]]
     row = [1]
     triangle = [[1]]
     for i in range(n-1):
-      row = [i+j for i,j in zip([0] + row, row + [0])]
-      triangle.append(row)
+        row = [i+j for i,j in zip([0] + row, row + [0])]
+        triangle.append(row)
         
     return triangle
 
